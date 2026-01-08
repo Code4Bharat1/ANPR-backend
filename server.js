ec263232680c509009fe900523a23f3bf10475c8
rlife <rlife0896@gmail.com>
2026-01-08 18:59:52 +0530

    feat: Update CORS configuration to allow webhooks.nexcorealliance.com

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ Global Middlewares
 ======================= */
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://anpr.nexcorealliance.com"], 
+    origin: ["http://localhost:3000","https://anpr.nexcorealliance.com","https://webhooks.nexcorealliance.com"], 
     credentials: true,
   })
 );
